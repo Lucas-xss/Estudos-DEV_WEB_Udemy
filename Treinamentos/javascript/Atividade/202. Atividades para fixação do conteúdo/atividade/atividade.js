9c84146665793af5df74b5b7800fafb93933713e
Lucas Xavier de Sousa dos Santos <94011359+Lucas-xss@users.noreply.github.com>
2024-01-08 14:42:45 -0300

    atividade concluída

--- Treinamentos/javascript/Atividade/202. Atividades para fixação do conteúdo/atividade/atividade.js
+++ Treinamentos/javascript/Atividade/202. Atividades para fixação do conteúdo/atividade/atividade.js
@@ -1,4 +1,28 @@
-function modificaEstilo (corDeFundo) {
+function ganha_foco () {
     document.getElementById('modificar').className = 'estilo1'
+    //document.getElementById('modificar').style.background = "yellow"
+}
+function perde_foco () {
+
+    //Seleciona o valor digitado
+    var recuperar = document.getElementById('modificar').value
+    //limpa o campo de digitação do imput
+    document.getElementById('modificar').value = ''
+    var minLength = 3
+    
+    if (recuperar.length < minLength){
+        document.getElementById('modificar').className = 'estilo2'
+    }else{
+        document.getElementById('modificar').className = 'estilo3'
+    }
+    console.log(recuperar)
+
+   /* var valorCampo = document.getElementById('modificar').value
+    if(valorCampo.length < 3) {
+    document.getElementById('modificar').style.background = "red"
+    } else {
+    document.getElementById('modificar').style.background = "green"
+    }*/
+   
 
 }
